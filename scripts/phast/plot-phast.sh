d5d07310e56ec333f82d4b74eed4f11ab5324fe9
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2025-10-04 09:36:47 +0200

    do not check number of threads; fixed pdf -> tex

--- scripts/phast/plot-phast.sh
+++ scripts/phast/plot-phast.sh
@@ -2,12 +2,12 @@
 
 ./sqlplot-tools ST50M.tex
 rm -f ST50Mprev.pdf
-pdflatex ST50Mprev.pdf
-pdflatex ST50Mprev.pdf
+pdflatex ST50Mprev.tex
+pdflatex ST50Mprev.tex
 
 ./sqlplot-tools MT50M.tex
 rm -f MT50Mprev.pdf
-pdflatex MT50Mprev.pdf
-pdflatex MT50Mprev.pdf
+pdflatex MT50Mprev.tex
+pdflatex MT50Mprev.tex
 
 rm -f *.out *.log *.aux
